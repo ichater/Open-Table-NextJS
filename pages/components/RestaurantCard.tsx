a47ec80c34da361f473f5d003c49ee66e1fa1ca7
ichater <chaterizaak@gmail.com>
2023-05-08 13:51:03 +1000

    after a lot of frustration i got the api calls working

--- pages/components/RestaurantCard.tsx
+++ pages/components/RestaurantCard.tsx
@@ -1,6 +1,12 @@
 import Link from "next/link";
+import { RestaurantCardType } from "..";
+import Restaurant from "../restaurant/[slug]";
 
-export default function RestaurantCard() {
+interface Props {
+  restaurant: RestaurantCardType;
+}
+
+export default function RestaurantCard({ restaurant }: Props) {
   return (
     <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
       <Link href="/restaurant/milestones-grill">
@@ -10,7 +16,7 @@ export default function RestaurantCard() {
           className="w-full h-36"
         />
         <div className="p-1">
-          <h3 className="font-bold text-2xl mb-2">Milestones Grill</h3>
+          <h3 className="font-bold text-2xl mb-2">{restaurant.name} adfsgah</h3>
           <div className="flex items-start">
             <div className="flex mb-2">*****</div>
             <p className="ml-2">77 reviews</p>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,52 @@
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
+import { prisma } from "../server/db/client";
+import { Cuisine, Location, PRICE } from "@prisma/client";
 
-export default function Home() {
+export type RestaurantCardType = {
+  id: number;
+  name: string;
+  main_image: string;
+  cuisine: Cuisine;
+  location: Location;
+  price: PRICE;
+};
+
+type Props = {
+  restaurants: RestaurantCardType[];
+};
+
+export default function Home({ restaurants }: Props) {
+  console.log({ restaurants });
   return (
     <main>
       <Header />
       <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-        <RestaurantCard />
+        {restaurants.map((restaurant) => (
+          <RestaurantCard restaurant={restaurant} />
+        ))}
       </div>
     </main>
   );
+}
+
+// export default Home;
+
+export async function getServerSideProps() {
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      cuisine: true,
+      location: true,
+      price: true,
+    },
+  });
+
+  return {
+    props: {
+      restaurants: JSON.parse(JSON.stringify(restaurants)),
+    },
+  };
 }
