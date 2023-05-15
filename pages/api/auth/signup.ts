2437f576a901521f7446b0097d46f828bd656eed
ichater <chaterizaak@gmail.com>
2023-05-15 12:26:40 +1000

    added create user functionality

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,5 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import validator from "validator";
+import { prisma } from "../../../server/db/client";
+import bcrypt from "bcrypt";
 
 export default async function handler(
   req: NextApiRequest,
@@ -46,8 +48,31 @@ export default async function handler(
       return res.status(400).json({ errorMessage: errors.join(" ") });
     }
 
+    const userWithEmail = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (userWithEmail) {
+      return res
+        .status(400)
+        .json({ errorMessage: "Email is associated with another account" });
+    }
+
+    const hashedPassword = await bcrypt.hash(passWord, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        first_name: firstName,
+        last_name: lastName,
+        password: hashedPassword,
+        city,
+        phone,
+        email,
+      },
+    });
+
     res.status(200).json({
-      hello: "{ ...body }",
+      hello: user,
     });
   }
 }
